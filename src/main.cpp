54566729ab638c20ffe05507181eb280d43b4c80
EscapeNumber001 <19634710+EscapeNumber001@users.noreply.github.com>
2024-08-28 10:43:21 -0400

    Initial commit

---
+++ src/main.cpp
@@ -0,0 +1,24 @@
+/*
+ * =====================================================================================
+ *
+ *       Filename:  main.cpp
+ *
+ *    Description:  
+ *
+ *        Version:  1.0
+ *        Created:  08/28/24 10:41:43
+ *       Revision:  none
+ *       Compiler:  gcc
+ *
+ *         Author:  YOUR NAME (), 
+ *   Organization:  
+ *
+ * =====================================================================================
+ */
+#include <iostream>
+
+int main()
+{
+  std::cout << "Hello world" << std::endl;
+  return 0;
+}
