b43de257dcdc6d0fd3f3a20f466cd8d878d62a8a
EscapeNumber001 <19634710+EscapeNumber001@users.noreply.github.com>
2024-09-01 13:30:06 -0400

    Added CSS styling to improve contrast

--- src/main.cpp
+++ src/main.cpp
@@ -16,12 +16,10 @@
  * =====================================================================================
  */
 
-// TODO: Replace TUI code with GTK or similar GUI interface
-
-
 #include <vector>
 #include "game.hpp"
 #include <gtk/gtk.h>
+#include <time.h>
 
 struct Point;
 class Board;
@@ -31,7 +29,7 @@ class Tile;
 #define FLAG_MODE 1
 
 const char* tileDisplayChars[] = {
-  "0",
+  " ",  // 0
   "1",
   "2",
   "3",
@@ -45,6 +43,8 @@ const char* tileDisplayChars[] = {
   "X"   // Revealed mine
 };
 
+std::vector<UITileButton*>* buttons;
+
 Point::Point(int x, int y)
 {
   this->x = x;
@@ -56,10 +56,17 @@ Point::Point()
   Point(0, 0);
 }
 
+UITileButton::UITileButton(GtkWidget* button, Tile* tile)
+{
+  this->button = button;
+  this->associatedTile = tile;
+}
+
 Tile::Tile(int x, int y, Board* board)
 {
   isMine = false;
   isRevealed = false;
+  isFlagged = false;
   surroundingMineCount = -1;  // This value is set by TryRevealTile().
   position = (Point){x, y};
   this->board = board;
@@ -92,20 +99,41 @@ void Tile::TryRevealTile()
     surroundingMineCount = CountAdjacentMines();
 
   isRevealed = true;
+  if (surroundingMineCount == 0)
+    RevealAdjacentZeroTiles();
 }
 
 // TODO: Implement this system more elegantly
 const char* Tile::GetDisplayCharacter()
 {
+  if (isFlagged)
+    return tileDisplayChars[9];
+
   if (!isRevealed)
     return tileDisplayChars[10];
 
   if (!isMine)
     return tileDisplayChars[CountAdjacentMines()];
-
-  // TODO: Flagged state
   
   return tileDisplayChars[11];
+}
+
+void Tile::RevealAdjacentZeroTiles()
+{
+  Tile* adjacentTiles[4] = {
+    board->GetTileAtPoint((Point){position.x + 1, position.y}),
+    board->GetTileAtPoint((Point){position.x - 1, position.y}),
+    board->GetTileAtPoint((Point){position.x, position.y + 1}),
+    board->GetTileAtPoint((Point){position.x, position.y - 1}),
+  };
+
+  for (Tile* t : adjacentTiles)
+  {
+    if (t == nullptr || t->isMine || t->isRevealed)
+      continue;
+
+    t->TryRevealTile();
+  }
 }
 
 // TODO: this needs re-indented
@@ -135,15 +163,26 @@ Board* b = NULL;
 GtkWidget* window;
 bool mode = REVEAL_MODE;
 
+void updateAllTileButtons(std::vector<UITileButton*>* buttons)
+{
+  for (UITileButton* b : *buttons)
+  {
+    gtk_button_set_label(GTK_BUTTON(b->button), b->associatedTile->GetDisplayCharacter());
+    if (b->associatedTile->isRevealed)
+    {
+      gtk_widget_set_sensitive(GTK_WIDGET(b->button), false);
+      gtk_style_context_add_class(GTK_STYLE_CONTEXT(gtk_widget_get_style_context(b->button)), "empty_tile");
+    }
+  }
+}
+
 void setModeToReveal(GtkButton* button, gpointer user_data)
 {
-  g_print("Something happened\n");
   mode = REVEAL_MODE;
 }
 
 void setModeToFlag(GtkButton* button, gpointer user_data)
 {
-  g_print("Something happened\n");
   mode = FLAG_MODE;
 }
 
@@ -154,7 +193,7 @@ static void revealTileGtk(GtkButton* btn, gpointer userdata)
 
   Tile* t = (Tile*)userdata;
   t->TryRevealTile();
-  gtk_button_set_label(GTK_BUTTON(btn), t->GetDisplayCharacter());
+  updateAllTileButtons(buttons);
 }
 
 
@@ -166,7 +205,12 @@ static void tileClicked(GtkButton* btn, gpointer userdata)
   }
   else if (mode == FLAG_MODE)
   {
-    // TODO
+    Tile* t = (Tile*)userdata;
+    if (t->isRevealed)
+      return;
+
+    t->isFlagged = !t->isFlagged;
+    gtk_button_set_label(GTK_BUTTON(btn), t->GetDisplayCharacter());
   }
 }
 
@@ -179,12 +223,17 @@ static void on_activate(GtkApplication* app) {
      g_clear_error(&error);
       return;
   }
+  GtkCssProvider* css = gtk_css_provider_new();
+  gtk_css_provider_load_from_path(css, "../interface_style.css", NULL);
+  gtk_style_context_add_provider_for_screen(gdk_screen_get_default(), GTK_STYLE_PROVIDER(css), GTK_STYLE_PROVIDER_PRIORITY_USER);
 
   window = GTK_WIDGET(gtk_builder_get_object(builder, "window"));
   gtk_window_set_application(GTK_WINDOW(window), app);
   GtkWidget* grid = GTK_WIDGET(gtk_builder_get_object(builder, "buttonsGrid"));
   g_signal_connect(GTK_WIDGET(gtk_builder_get_object(builder, "flagModeButton")), "clicked", G_CALLBACK(setModeToFlag), NULL);  // TODO: Connect these from Glade directly because it's a bit hacky doing it this way
   g_signal_connect(GTK_WIDGET(gtk_builder_get_object(builder, "revealModeButton")), "clicked", G_CALLBACK(setModeToReveal), NULL);
+
+  buttons = new std::vector<UITileButton*>;
   for (Tile* t : b->tiles)
   {
     Point pos = t->GetPosition();
@@ -192,6 +241,9 @@ static void on_activate(GtkApplication* app) {
     gtk_widget_set_visible(button, true);
     g_signal_connect(button, "clicked", G_CALLBACK(tileClicked), t);
     gtk_grid_attach(GTK_GRID(grid), button, pos.x, pos.y, 1, 1);
+
+    UITileButton* newButton = new UITileButton(button, t);
+    buttons->push_back(newButton);
   }
   gtk_window_present(GTK_WINDOW(window));
 
@@ -217,17 +269,30 @@ static void on_activate(GtkApplication* app) {
 
 int main(int argc, char* argv[])
 {
+  srand(time(NULL));
   b = new Board();
-  for (int y = 0; y < 5; y++)
+
+  int board_size_x = 10;
+  int board_size_y = 10;
+  int num_mines = 4;
+
+  // Create tiles
+  for (int y = 0; y < board_size_y; y++)
   {
-    for (int x = 0; x < 5; x++)
+    for (int x = 0; x < board_size_x; x++)
     {
-      int randomNum = rand() % 4;
       Tile* t = new Tile(x, y, b);
-      t->isMine = randomNum == 1;
       b->tiles.push_back(t);
     }
   }
+
+  int square_size = board_size_x * board_size_y;
+  for (int i = 0; i < num_mines; i++)
+  {
+    int index = rand() % square_size;
+    b->tiles[index]->isMine = true;
+  }
+
 
   GtkApplication* app = gtk_application_new("com.EscapeNumber001.Minesweeper", \
       G_APPLICATION_DEFAULT_FLAGS);
